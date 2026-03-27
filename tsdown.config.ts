f108e9c4650535bffab1934377a99ef2ac7d2fb6
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-03-27 10:45:34 +0100

    Update dependencies and move to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,20 +1,21 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
 const pdfiumWasmHash = process.env.PDFIUM_WASM_HASH ?? '';
 const tesseractCdnUrl = process.env.TESSERACT_CDN_URL ?? '';
 
 export default defineConfig({
   dts: true,
-  splitting: true,
   sourcemap: true,
   clean: true,
-  metafile: true,
   format: 'esm',
   entry: ['src/index.ts'],
-  noExternal: [/^@embedpdf/],
-  external: ['react', 'react-dom', /^@navikt\//, /^@opentelemetry\//],
   define: {
     __PDFIUM_WASM_HASH__: JSON.stringify(pdfiumWasmHash),
     __TESSERACT_CDN_URL__: JSON.stringify(tesseractCdnUrl),
   },
+  deps: {
+    neverBundle: ['react', 'react-dom', /^@navikt\//, /^@opentelemetry\//],
+    alwaysBundle: [/^@embedpdf/],
+  },
+  target: false,
 });
