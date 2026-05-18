0b7a0129a5396fa6d0eb1c33910fa8a49a7e6fbf
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-05-18 15:55:11 +0200

    v1.2.5 - Add background to rotate button

--- src/files/rotate-button.tsx
+++ src/files/rotate-button.tsx
@@ -1,29 +1,23 @@
 import { RotateLeftIcon } from '@navikt/aksel-icons';
-import { Button, Theme, Tooltip } from '@navikt/ds-react';
-import { ThemeMode, useFileViewerConfig } from '@/context';
+import { Button, Tooltip } from '@navikt/ds-react';
 
 interface RotateButtonProps {
   pageNumber: number;
   onRotate: () => void;
 }
 
-export const RotateButton = ({ pageNumber, onRotate }: RotateButtonProps) => {
-  const { invertColors, theme } = useFileViewerConfig();
-  const isPageDark = invertColors && theme === ThemeMode.Dark;
-
-  return (
-    <Theme theme={isPageDark ? undefined : 'light'} className="absolute top-2 left-2 z-10">
-      <Tooltip content="Roter til venstre / mot klokken" placement="right" describesChild>
-        <Button
-          type="button"
-          size="xsmall"
-          variant="tertiary"
-          onClick={onRotate}
-          data-color="neutral"
-          icon={<RotateLeftIcon aria-hidden />}
-          aria-label={`Roter side ${pageNumber.toString(10)} mot klokken`}
-        />
-      </Tooltip>
-    </Theme>
-  );
-};
+export const RotateButton = ({ pageNumber, onRotate }: RotateButtonProps) => (
+  <div className="absolute top-2 left-2 z-10">
+    <Tooltip content="Roter til venstre / mot klokken" placement="right" describesChild>
+      <Button
+        type="button"
+        size="xsmall"
+        variant="primary"
+        onClick={onRotate}
+        data-color="neutral"
+        icon={<RotateLeftIcon aria-hidden />}
+        aria-label={`Roter side ${pageNumber.toString(10)} mot klokken`}
+      />
+    </Tooltip>
+  </div>
+);
